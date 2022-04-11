ab6d8494339b719ca83b628e1f80f45c482e46d6
Elena <konyshevael@rambler.ru>
2022-04-11 17:33:18 +0500

    lesson1+ webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,69 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-    entry: path.resolve(__dirname, '.src/index.jsx')
+    entry: path.resolve(__dirname, './src/index.jsx'),
+    output: {
+        filename: "./bundle.js",
+        path: path.resolve(__dirname, './build')
+    },
+    resolve: {
+        extensions: ['.jsx', '.js', '.css',]
+    },
+    devtool: 'eval-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.css$/i,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+    ]
 }
