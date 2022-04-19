93f93feb34a46484f4069bac06db18975ddf0bd2
Elena <konyshevael@rambler.ru>
2022-04-19 13:26:20 +0500

    change test

--- src/componentFunc/Messages/components/InputText/InputText.jsx
+++ src/componentFunc/Messages/components/InputText/InputText.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 export const InputText = (props) => {
   return (
-    <textarea data-testid="custom-element"
+    <textarea
+      data-testid="custom-element"
       placeholder="write your message"
       onChange={props.changeText}
       value={props.value}
